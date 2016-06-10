f683f1838f86f6d9ced9d82ca379a1ba10716c64
Jan Vansteenkiste <jan@vstone.eu>
2016-06-10 08:30:57 +0200

    update spec tests to newer style (fix deprecation warnings)

--- spec/functions/hash_keys_spec.rb
+++ spec/functions/hash_keys_spec.rb
@@ -2,33 +2,22 @@ require 'spec_helper'
 
 describe 'hash_keys' do
 
-  let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
-
-  it 'should exist' do
-    Puppet::Parser::Functions.function(:hash_keys).should == 'function_hash_keys'
-  end
-
   describe 'should throw an error with' do
+
     it 'no arguments' do
-      lambda {
-        scope.function_hash_keys([])
-      }.should raise_error(Puppet::ParseError, /requires 1 argument/)
+      is_expected.to run.with_params().and_raise_error(Puppet::ParseError, /requires 1 argument/)
     end
 
     it 'more than 1 arguments' do
-      lambda {
-        scope.function_hash_keys(['foo','bar'])
-      }.should raise_error(Puppet::ParseError, /requires 1 argument/)
+      is_expected.to run.with_params('foo','bar').and_raise_error(Puppet::ParseError, /requires 1 argument/)
     end
 
     it 'wrong argument type' do
-      lambda {
-        scope.function_hash_keys(['foo'])
-      }.should raise_error(Puppet::ParseError, /Argument is not an hash/)
+      is_expected.to run.with_params('foo').and_raise_error(Puppet::ParseError, /Argument is not an hash/)
     end
   end
 
   it 'should return hash keys' do
-    scope.function_hash_keys([{'foo' => 'somehting', 'bar' => []}]).sort.should == ['foo','bar'].sort
+    is_expected.to run.with_params({'foo' => 'somehting', 'bar' => []}).and_return(['foo','bar'])
   end
 end
--- spec/functions/select_users_spec.rb
+++ spec/functions/select_users_spec.rb
@@ -2,55 +2,35 @@ require 'spec_helper'
 
 describe 'select_users' do
 
-  let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
-
-  it 'should exist' do
-    Puppet::Parser::Functions.function(:select_users).should == 'function_select_users'
-  end
-
   describe 'should throw an error with' do
     it 'less than 3 arguments' do
-      lambda {
-        scope.function_select_users(['test', {}])
-      }.should raise_error(Puppet::ParseError, /requires 3 or 4 arguments/)
+      is_expected.to run.with_params('test', {}).and_raise_error(Puppet::ParseError, /requires 3 or 4 arguments/)
     end
 
     it 'more than 4 arguments' do
-      lambda {
-        scope.function_select_users(['test', {}, {}, {}, 'foo'])
-      }.should raise_error(Puppet::ParseError, /requires 3 or 4 arguments/)
+      is_expected.to run.with_params('test', {}, {}, {}, 'foo').and_raise_error(Puppet::ParseError, /requires 3 or 4 arguments/)
     end
 
     describe 'wrong type for' do
       it 'the users argument' do
-        lambda {
-          scope.function_select_users([false, {}, {}])
-        }.should raise_error(Puppet::ParseError, /first argument \(users\) is not a/)
+        is_expected.to run.with_params(false, {}, {}).and_raise_error(Puppet::ParseError, /first argument \(users\) is not a/)
       end
 
       it 'the user_uids argument' do
-        lambda {
-          scope.function_select_users(['foo', 'bar', {}])
-        }.should raise_error(Puppet::ParseError, /second argument \(user_uids\) is not a/)
+        is_expected.to run.with_params('foo', 'bar', {}).and_raise_error(Puppet::ParseError, /second argument \(user_uids\) is not a/)
       end
 
       it 'the user_info argument' do
-        lambda {
-          scope.function_select_users(['foo', {}, 'bar'])
-        }.should raise_error(Puppet::ParseError, /third argument \(user_info\) is not a/)
+        is_expected.to run.with_params('foo', {}, 'bar').and_raise_error(Puppet::ParseError, /third argument \(user_info\) is not a/)
       end
 
       it 'the user_default argument' do
-        lambda {
-          scope.function_select_users(['foo', {}, {}, 'bar'])
-        }.should raise_error(Puppet::ParseError, /fourth argument \(user_defaults\) is not a/)
+        is_expected.to run.with_params('foo', {}, {}, 'bar').and_raise_error(Puppet::ParseError, /fourth argument \(user_defaults\) is not a/)
       end
     end
 
     it 'when the user is not found in user_uids' do
-      lambda {
-        scope.function_select_users(['foo', {}, {}])
-      }.should raise_error(Puppet::ParseError, /is not found in user_uids/)
+      is_expected.to run.with_params('foo', {}, {}).and_raise_error(Puppet::ParseError, /is not found in user_uids/)
 
     end
   end
@@ -61,24 +41,24 @@ describe 'select_users' do
     let (:user_uids) { { 'foo' => '1000',} }
     let (:user_info) { { 'foo' => { 'foobar' => 'something' } } }
     it do
-      scope.function_select_users([users, user_uids, user_info]).should == { 'foo' => {
+      is_expected.to run.with_params(users, user_uids, user_info).and_return({'foo' => {
         'uid' => '1000',
         'foobar' => 'something',
-      } }
+      }})
     end
   end
   context 'with an array of users' do
     let (:users) { ['foo','bar'] }
     let (:user_uids) { { 'foo' => '1000', 'bar' => '1001','funky' => '1002' } }
     it do
-      scope.function_select_users([users, user_uids, {}, {}]).should == {'foo' => {'uid' => '1000', }, 'bar' => {'uid' => '1001', } }
+      is_expected.to run.with_params(users, user_uids, {}, {}).and_return({'foo' => {'uid' => '1000', }, 'bar' => {'uid' => '1001', } })
     end
   end
   context 'with user defaults' do
     let (:user_uids) { { 'foo' => '1000', 'bar' => '1001','funky' => '1002' } }
     let (:user_defaults) { { 'shell' => '/bin/bash' } }
     it do
-      scope.function_select_users([['foo'], user_uids, {}, user_defaults]).should == { 'foo' => {'uid' => '1000', 'shell' => '/bin/bash' } }
+      is_expected.to run.with_params(['foo'], user_uids, {}, user_defaults).and_return({ 'foo' => {'uid' => '1000', 'shell' => '/bin/bash' } })
     end
   end
 
