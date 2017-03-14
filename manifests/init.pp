22656804c5fc036a3caed12ab3200183d47ae48f
Jan Vansteenkiste <jan@vstone.eu>
2017-03-14 12:07:33 +0100

    We no longer use hiera_(array|hash) functions. hiera 5 does the work for us

--- manifests/init.pp
+++ manifests/init.pp
@@ -25,12 +25,6 @@
 #   from the system. This removes the configured ssh keys from the
 #   users homefolder.
 #
-# [*hiera_merge*]:
-#   When specifying users and groups on multiple levels in a hiera
-#   hierarchy, set this to true to refetch the parameters with
-#   either hiera_array or hiera_hash.
-#
-#
 class accounts (
   Array[String]       $groups        = [],
   Array[String]       $users         = [],
@@ -38,35 +32,22 @@ class accounts (
   Hash[String,Hash]   $user_info     = {},
   Hash                $user_defaults = {},
   Boolean             $purge         = false,
-  Boolean             $hiera_merge   = false,
 ) {
-
-  if $hiera_merge {
-    $_groups    = hiera_array('accounts::groups', [])
-    $_users     = hiera_array('accounts::users', [])
-    $_user_uids = hiera_hash('accounts::user_uids', {})
-    $_user_info = hiera_hash('accounts::user_info', {})
-  } else {
-    $_groups    = $groups
-    $_users     = $users
-    $_user_uids = $user_uids
-    $_user_info = $user_info
-  }
 
   group {$groups:
     ensure => 'present',
   }
 
-  $create_users = select_users($_users, $_user_uids, $_user_info, $user_defaults)
+  $create_users = select_users($users, $user_uids, $user_info, $user_defaults)
   create_resources('accounts::hiera', $create_users)
 
   if $purge {
     ## Gets all users that have a uid configured.
-    $all_users = hash_keys($_user_uids)
+    $all_users = hash_keys($user_uids)
     ## Get the users that are not in the users array.
-    $users_absent = array_substract($all_users, $_users)
+    $users_absent = array_substract($all_users, $users)
     ## Create a big hash containing user information (but then absent...)
-    $absent_users = select_users($users_absent, $_user_uids, {}, {'ensure' => 'absent'})
+    $absent_users = select_users($users_absent, $user_uids, {}, {'ensure' => 'absent'})
     create_resources('accounts::hiera', $absent_users)
   }
 
